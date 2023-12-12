7e9e6d1c48c66e67cd5d774750c4ea74cc4719c0
JXJ101 <142345298+JXJ101@users.noreply.github.com>
2023-12-12 13:51:02 -0500

    Update oop.py

--- 06_OOP/oop.py
+++ 06_OOP/oop.py
@@ -1,4 +1,14 @@
 # Object-Oriented Programming, jahreem jeffers, V0.0
 
 class Person: # Use pascalCase fro ClassNames
+    def __init__(self, name, age, weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
     
+    # To String Function -- How the object appears as a string.
+    def __str__(self):
+        return f"Name: {self.name}\nThis person is {self.age} years old.\n They weigh {self.weight} pounds.\n"
+
+
+person1 = person("aiden redfield", 25, 201)
