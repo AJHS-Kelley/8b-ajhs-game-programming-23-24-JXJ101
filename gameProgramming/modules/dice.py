378cb837f8dbbfb88d08b2e4e32f0084f45fc083
JXJ101 <142345298+JXJ101@users.noreply.github.com>
2023-12-14 13:36:18 -0500

    dice

---
+++ gameProgramming/modules/dice.py
@@ -0,0 +1,13 @@
+# Dice rolling module by Jahreem Jeffers, V0.0
+import random
+
+def rollDice(numDice, sizeDice):
+    numRolled = 0
+	sum = 0
+    while numRolled < numDice
+	    roll = random.randint(1, sizeDice)
+		sum += roll
+		print(f"Roll:{roll}\n")
+		print(f"sum:{sum}\n")
+		numRolled += 1
+	return sum 
