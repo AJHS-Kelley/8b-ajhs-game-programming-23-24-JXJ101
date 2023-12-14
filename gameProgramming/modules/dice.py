4c80809042a2a3c6b9092cc0c6420d976bc29d70
JXJ101 <142345298+JXJ101@users.noreply.github.com>
2023-12-14 14:09:11 -0500

    j
    dice exploded

--- gameProgramming/modules/dice.py
+++ gameProgramming/modules/dice.py
@@ -1,7 +1,7 @@
 # Dice rolling module by Jahreem Jeffers, V0.0
 import random
 
-def roll(numDice, sizeDice):
+def roll(numDice, sizeDice): # Verified working 12/14/23
     numRolled = 0
 	sum = 0
     while numRolled < numDice
@@ -20,4 +20,13 @@ def display(numDice, sizeDice):
 		print(f"sum:{sum}\n")
 		numRolled += 1
 	return sum
+
+def isDoubles(roll1, roll2):
+    if roll1 == roll2:
+        isDoubles =  True
+        else:
+            isDoubles = false
+        return isDoubles
+
+
 
--- gameProgramming/modules/tester.py
+++ gameProgramming/modules/tester.py
@@ -1,5 +1,23 @@
 import dice
 
 
-dice.roll(1,6)
-dice.display(1,6)
+roll1 = dice.display(1,6)
+roll2 = dice.display(1,6)
+
+if dice.isExploding(roll1, 6):
+    print(roll1)
+    print("This roll exploded\n")
+    roll1 += dive.roll(1,6)
+    print(roll1)
+
+if dice.isDoubles(roll1, roll2):
+    print("You rolled a double go again!\n")
+else:
+    print("It was not a double.\n")
+
+def isExploding (roll, sizeRoll):
+    if roll == sizeRoll:
+        isExploding = True
+    else:
+        isExploding = False
+    return isExploding
