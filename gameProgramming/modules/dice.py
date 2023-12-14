c681f4a7f88f00aa448601b9355b07a91411bc15
JXJ101 <142345298+JXJ101@users.noreply.github.com>
2023-12-14 13:41:02 -0500

    Update dice.py
    one has print and one doesn't

--- gameProgramming/modules/dice.py
+++ gameProgramming/modules/dice.py
@@ -1,7 +1,16 @@
 # Dice rolling module by Jahreem Jeffers, V0.0
 import random
 
-def rollDice(numDice, sizeDice):
+def roll(numDice, sizeDice):
+    numRolled = 0
+	sum = 0
+    while numRolled < numDice
+	    roll = random.randint(1, sizeDice)
+		sum += roll
+		numRolled += 1
+	return sum
+
+def display(numDice, sizeDice):
     numRolled = 0
 	sum = 0
     while numRolled < numDice
@@ -10,4 +19,5 @@ def rollDice(numDice, sizeDice):
 		print(f"Roll:{roll}\n")
 		print(f"sum:{sum}\n")
 		numRolled += 1
-	return sum 
+	return sum
+
