ca036e24cea1b8029d0c39a61797a304caadf933
JXJ101 <142345298+JXJ101@users.noreply.github.com>
2024-03-31 16:18:25 -0400

    added card value

--- gameProgramming/09_finalProject/finalProject.py
+++ gameProgramming/09_finalProject/finalProject.py
@@ -20,6 +20,12 @@ if difficulty == 1:
 else:
     pygame.display.set_caption('jackblack -- XFACTOR')
 
+# Define card values
+card_values = {'2': 2, '3':3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
+
+# Define a class for a deck of cards
+
+
 
 
 
