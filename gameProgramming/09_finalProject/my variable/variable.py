66de268dd33ed5e2db14a975009918a8e6c5c0cb
JXJ101 <142345298+JXJ101@users.noreply.github.com>
2024-04-23 13:47:16 -0400

    ye

---
+++ gameProgramming/09_finalProject/my variable/variable.py
@@ -0,0 +1,14 @@
+# This is a method for testing code and preventing crashes.
+# try -- except -- else -- finally
+
+try: # The code in this block is always executed.
+    myVariable = 1
+    print(myVariabl)
+except NameError: # This code will run if there is an error (exception raised)
+    print("fix your spelling nig-")
+except:
+    print("something else went wrong my B")
+else: # if the code runs with no problem
+    print("i was hoping you were to screw up again.\n")
+finally: # THIS CODE ALWAYS RUNS, IT'S LIEK THE TERMINATOR
+    print("imortality bi###.\n")
