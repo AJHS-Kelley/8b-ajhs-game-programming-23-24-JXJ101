9a715639b3234e9fd461cc76facd117aceefbdab
JXJ101 <142345298+JXJ101@users.noreply.github.com>
2024-04-25 13:34:28 -0400

    Update variable.py

--- gameProgramming/09_finalProject/my variable/variable.py
+++ gameProgramming/09_finalProject/my variable/variable.py
@@ -3,12 +3,14 @@
 
 try: # The code in this block is always executed.
     myVariable = 1
-    print(myVariabl)
+    print(myVariable)
+    myString = "FIVE"
+    print(float(myString))
 except NameError: # This code will run if there is an error (exception raised)
     print("fix your spelling nig-")
-except:
-    print("something else went wrong my B")
+#except:
+    #print("something else went wrong my B")
 else: # if the code runs with no problem
     print("i was hoping you were to screw up again.\n")
-finally: # THIS CODE ALWAYS RUNS, IT'S LIEK THE TERMINATOR
-    print("imortality bi###.\n")
+#finally: # THIS CODE ALWAYS RUNS, IT'S LIEK THE TERMINATOR
+    #print("imortality bi###.\n")
